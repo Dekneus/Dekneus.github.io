298df0a21ae0791f48a7614c3eaa884ab57fc6c0
Dekneus <144318443+Dekneus@users.noreply.github.com>
2023-09-19 08:48:44 +0200

    Added image to Hello World

--- script.js
+++ script.js
@@ -1,27 +1,59 @@
-const buttons = document.querySelectorAll('button');
-const topicTitle = document.getElementById('Title');
-const topicDescription = document.getElementById('Desc');
+const buttons = document.querySelectorAll('.button-container:not(.header-buttons) button');
+const VakTitel = document.getElementById('Title');
+const VakDescription = document.getElementById('Desc');
 
 const buttonContent = [
     { title: "LOB", description: "LOB is een chill vak" },
-    { title: "2", description: "fa" },
-    { title: "3", description: "fb" },
-    { title: "4", description: "fc" },
-    { title: "5", description: "fd" },
-    { title: "6", description: "fe" },
-    { title: "7", description: "ff" },
-    { title: "8", description: "fg" },
-    { title: "9", description: "fh" },
-    { title: "10", description: "fi" },
-    { title: "11", description: "fj" },
-    { title: "12", description: "fk" },
-    { title: "13", description: "fl" },
-    { title: "14", description: "fm" },
+    { title: "Database", description: "Databse belangrijk vak" },
+    { title: "Engels", description: "Engels is een makkelijk vak" },
+    { title: "GIT", description: "GIT.. We hebben het niet over GIT.." },
+    { title: "HTML/CSS", description: "HTML/CSS is een episch vak" },
+    { title: "IPFS", description: "IPFS is een bijzonder vak" },
+    { title: "Linux", description: "Linux is een vak voor Discord moderators" },
+    { title: "Nederlands", description: "Nederlands is een kwalitatief uitermate teleurstellend vak" },
+    { title: "Rekenen", description: "Rekenen.. bro ik heb geen zin om terug te gaan naar groep 8 ;(" },
+    { title: "SCRUM", description: "SCRUM is ok vak I guess" },
+    { title: "SLB", description: "SLB ik begin carpeltunnel te krijgen help" },
+    { title: "Zakelijke Communicatie", description: "Zakelijke communicatie. Waarom zit dit niet bij Nederlands..?" },
+    { title: "Burgerschap", description: "Burgerschap is het matigste vak" },
+    { title: "Greenfoot", description: "Greenfoot is gamemaker voor volwassenen" },
 ];
+
+let selectedButton = null;
 
 buttons.forEach((button, index) => {
     button.addEventListener('click', () => {
-        topicTitle.textContent = buttonContent[index].title;
-        topicDescription.textContent = buttonContent[index].description;
+        if (selectedButton) {
+            selectedButton.style.backgroundColor = "#bbbbbb";
+            selectedButton.style.transform = "scale(1)";
+        }
+
+        button.style.backgroundColor = getRandomColor();
+        button.style.transform = "scale(1.1)";
+        selectedButton = button;
+
+        VakTitel.textContent = buttonContent[index].title;
+        VakDescription.textContent = buttonContent[index].description;
+    });
+
+    button.addEventListener('mouseover', () => {
+        if (button !== selectedButton) {
+            button.style.backgroundColor = getRandomColor();
+        }
+    });
+
+    button.addEventListener('mouseout', () => {
+        if (button !== selectedButton) {
+            button.style.backgroundColor = "#bbbbbb";
+        }
     });
 });
+
+function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
